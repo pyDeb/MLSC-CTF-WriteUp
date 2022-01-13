6bff0fffbf5f144e2d4017fe878a255128b0d192
Masoud Sadeghi <35397912+pyDeb@users.noreply.github.com>
2022-01-13 14:41:46 -0500

    Create D0cum3n7s.py

---
+++ D0cum3n7s.py
@@ -0,0 +1,11 @@
+fl = "ZB()_+:?FGHJ{KsL[];',.AlSD34QXCVWEmRTYU#$%^&*IOP12NM5c67}890-=/!@"
+flag = ""
+
+
+flag = fl[34] + fl[23] + fl[14] + fl[53] + fl[12] + fl[1] + fl[27] + \
+        fl[24] + fl[45] + fl [29] + fl[4] + fl[51] + fl[64] + fl[30] + fl[34] +>
+        fl[35] + fl[59] + fl[56]
+
+
+print(flag)
+
